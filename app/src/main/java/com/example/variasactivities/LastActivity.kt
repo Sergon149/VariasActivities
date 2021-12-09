a87e0ab551bbba6f97c1b2735a4c2882341bc735
Sergio <sergon149@alu.centroafuera.es>
2021-12-09 14:06:04 +0100

    actualizado

--- app/src/main/java/com/example/variasactivities/LastActivity.kt
+++ app/src/main/java/com/example/variasactivities/LastActivity.kt
@@ -1,7 +1,10 @@
 package com.example.variasactivities
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import com.example.variasactivities.databinding.ActivityLastBinding
 import com.example.variasactivities.databinding.ActivitySecondBinding
 
@@ -9,12 +12,23 @@ class LastActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLastBinding
 
+    companion object{
+
+        const val Tag_Text = "Tag_Text"
+
+        fun launch(context: Context, text: String){
+            val intent = Intent(context, LastActivity::class.java)
+            intent.putExtra(Tag_Text, text)
+            context.startActivity(intent)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLastBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val initialData2 = intent.getStringExtra("TEXT")
-        binding.tvTexto.text = "$initialData2"
+        val initialData2 = intent.getStringExtra(Tag_Text)
+        binding.tvTexto.text = initialData2
     }
 }
--- app/src/main/java/com/example/variasactivities/SecondActivity.kt
+++ app/src/main/java/com/example/variasactivities/SecondActivity.kt
@@ -4,11 +4,12 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.variasactivities.databinding.ActivitySecondBinding
-import kotlin.random.Random
 
 class SecondActivity : AppCompatActivity(){
 
     private lateinit var binding: ActivitySecondBinding
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,9 +20,7 @@ class SecondActivity : AppCompatActivity(){
         binding.tvText2.text = "$initialData"
 
         binding.button2.setOnClickListener {
-            val intent = Intent(this, LastActivity::class.java)
-            intent.putExtra("TEXT", binding.tvText2.text)
-            startActivity(intent)
+            LastActivity.launch(this,binding.tvText2.text.toString())
         }
 
 
